59bfa8da67fa0ff01a83fc347f00f1bef26a553c
FreakyTozex <antoniodepinho0611@gmail.com>
2026-05-18 09:39:31 +0100

    Alterar settings para utilizar o .env e instalar dependencias

--- Rede-Social/socialapp/settings.py
+++ Rede-Social/socialapp/settings.py
@@ -11,21 +11,32 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+  # Carrega as variáveis de ambiente do arquivo .env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(os.path.join(BASE_DIR, '../.env'))
+
+env = environ.Env(
+    DEBUG=(bool, False),
+    SECRET_KEY=(str, "django-insecure-default-key-do-not-use"),
+)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w6q#k$psz)iqx%y11&z^gtjodwk58((p2=#fb$$@0hn0if9$vx'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost',]
 
 
 # Application definition
